6a578f9e0560a9456ab860673c5f269bb4b435ee
Rajnish <rajnishmsrit@gmail.com>
2017-08-04 08:29:33 -0700

    Script to create drupal profile from single option

---
+++ drupal-profile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./dsql.sh $1
+drush dl drupal-7.x --drupal-project-rename=$1
+cd $1
+drush site-install standard --db-url=mysql://$1:$1@localhost/$1 --site-name=Welcome --account-name=admin --account-pass=$1
