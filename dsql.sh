667f24bfd095ec275e1cc6a55e2738c6540136d4
Rajnish <rajnishmsrit@gmail.com>
2017-08-06 23:50:46 -0700

    Added cleanup before creation

--- dsql.sh
+++ dsql.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Clean up user and database
+mysql -u root -proot -e "DROP USER IF EXISTS '$1'@'localhost'"
+mysql -u root -proot -e "DROP DATABASE IF EXISTS $1"
+
+# Create database, user, grant privileges
 mysql -u root -proot -e "create database $1"
 mysql -u root -proot -e "create user '$1'@localhost identified by '$1'"
 mysql -u root -proot -e "GRANT ALL PRIVILEGES ON $1.* TO '$1'@localhost WITH GRANT OPTION"
